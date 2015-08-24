f52bfde821c9bb8e4505eb0f62b313b59b18b64b
Inti Logo <emilh8@gmail.com>
2015-08-24 13:09:41 -0400

    gps inti add
    Wanted to play with gps vehicle images and lightbox

---
+++ gpsvehicletrackingdevise.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!-- DW6 -->
+<head>
+<title>GPS Vehicle Tracking Devise</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="styles/vehicletracking.css" type="text/css" />
+<script language="JavaScript" type="text/javascript">
+//--------------- LOCALIZEABLE GLOBALS ---------------
+var d=new Date();
+var monthname=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+//Ensure correct for language. English is "January 1, 2004"
+var TODAY = monthname[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+//---------------   END LOCALIZEABLE   ---------------
+</script>
+</head>
+<body>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<tr><td>
+<table width="800" border="1" cellspacing="1" cellpadding="0" align="center">
+  <tr bgcolor="#FFFFFF" >
+    <td colspan="3" rowspan="2" bordercolor="#FFFFFF" ><img src="images/intigps.jpg" alt="Header image" width="318" height="101" border="0" /></td>
+    <td height="50" colspan="3" align="center" valign="bottom" nowrap="nowrap" bordercolor="#FFFFFF" id="logo">GPS Vehicle Tracking </td>
+    <td width="1" bordercolor="#FFFFFF">&nbsp;</td>
+  </tr>
+
+  <tr bgcolor="#FFFFFF" border="0">
+    <td height="51" colspan="3" align="center" valign="top" bordercolor="#FFFFFF" id="tagline">Fleet Tracking System Experts </td>
+	<td width="1" bordercolor="#FFFFFF">&nbsp;</td>
+  </tr>
+
+  <tr>
+    <td colspan="7" bgcolor="#5C743D"><img src="images/gps.gif" alt="" width="1" height="2" border="0" /></td>
+  </tr>
+
+  <tr>
+    <td colspan="7" bgcolor="#99CC66" background="images/fleet_line.gif"><img src="images/fleet_line.gif" alt="line decor" width="4" height="3" border="0" /></td>
+  </tr>
+
+  <tr bgcolor="#99CC66">
+  	<td height="20" colspan="7" bgcolor="#000000" id="dateformat">&nbsp;&nbsp;
+    <script language="JavaScript" type="text/javascript">
+      document.write(TODAY);	</script></td>
+  </tr>
+  <tr>
+    <td colspan="7" bgcolor="#99CC66" background="images/fleet_line.gif"><img src="images/fleet_line.gif" alt="line decor" width="4" height="3" border="0" /></td>
+  </tr>
+
+  <tr>
+    <td colspan="7" bgcolor="#660000"><img src="images/gps.gif" alt="" width="1" height="2" border="0" /></td>
+  </tr>
+
+ <tr>
+    <td width="298" valign="top" bgcolor="#660000">
+	<?php
+require("sidenav.html");
+?>
+		 <br />
+  	&nbsp;<br />
+  	&nbsp;<br />
+  	&nbsp;<br /> 	</td>
+    <td width="16" bordercolor="#FFFFFF" bgcolor="#FFFFFF"><img src="images/gps.gif" alt="" width="15" height="1" border="0" /></td>
+    <td colspan="2" valign="top" bordercolor="#FFFFFF"><img src="images/gps.gif" alt="" width="305" height="1" border="0" /><br />
+	
+	&nbsp;<br />
+	<table width="364" border="0" cellpadding="0" cellspacing="0" bordercolor="#FFFFFF">
+        <tr>
+          <td width="364" class="pageName"><h1>GPS Vehicle Tracking Device</h1></td>
+		</tr>
+
+		<tr>
+          <td class="bodyText"><p><img src="images/trip_example.jpg" width="313" height="402" /></p>
+            <p>Our GPS vehicle tracking systems reports the exact position   and speed of a vehicle. This is unlike most other systems which do not exactly   show the location of a vehicle. The map below shows the vehicle turning. The    vehicle unit will automatically record more route indicators when a vehicle is   changing speed or direction quickly. This makes our solutions exceptionally powerful   and gives the best possible detail.</p>
+            </td>
+        </tr>
+      </table>
+	 <br />
+	&nbsp;<br />	</td>
+    <td width="20" bordercolor="#FFFFFF"><img src="images/gps.gif" alt="" width="20" height="1" border="0" /></td>
+        <td width="257" valign="top">
+		<table border="0" cellspacing="0" cellpadding="0" width="225" id="leftcol">
+
+	   <tr>
+       <td width="10"><img src="images/gps.gif" alt="" width="10" height="1" border="0" /></td>
+<?php
+require("rightnav.html");
+?>         <td width="21">&nbsp;</td>
+        </tr>
+      </table>	</td>
+	<td width="1">&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="7">&nbsp;</td>
+  </tr>
+</table>
+</td></tr>
+</table>
+<!-- BEGIN WebSTAT Activation Code -->
+<script type="text/javascript" language="JavaScript" src="http://hits.nextstat.com/cgi-bin/wsv2.cgi?102020"></script>
+<noscript>
+<a href="http://www.webstat.com" rel="nofollow">
+<img src="http://hits.nextstat.com/scripts/wsb.php?ac=102020" border="0" alt="Web Analytics and Web Statistics by NextSTAT" /></a>
+</noscript>
+<!-- END WebSTAT Activation Code -->
+</body>
+</html>
